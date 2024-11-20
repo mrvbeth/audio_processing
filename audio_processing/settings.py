ed86cf9ce57292f4ef508d228c37fcfde5959a7f
mrvbeth <145864337+mrvbeth@users.noreply.github.com>
2024-11-21 00:21:45 +0100

    commit 5

--- audio_processing/settings.py
+++ audio_processing/settings.py
@@ -120,10 +120,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-# MEDIA CONFIGURATION
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
