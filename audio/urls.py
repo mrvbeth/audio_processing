02be55689813bb2a089a3f95527489d45f2f0a09
mrvbeth <145864337+mrvbeth@users.noreply.github.com>
2024-11-21 00:32:53 +0100

    commit 6

--- audio/urls.py
+++ audio/urls.py
@@ -7,4 +7,3 @@ urlpatterns = [
     path('upload/', views.upload_audio, name='upload_audio'),
     path('download/<str:filename>/', views.download_audio, name='download_audio'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
